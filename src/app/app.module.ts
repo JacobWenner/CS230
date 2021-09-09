595fafac596e07d5d4a0308bb208890741db0795
JacobWenner <jwenner13@hotmail.com>
2021-09-09 18:30:38 -0400

    CSS formatting on navbars, implemented cards and offcanvas searchbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card1/card1.component';
 import { FooterComponent } from './footer/footer.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
 
@@ -10,6 +11,7 @@ import { TopNavBarComponent } from './header/top-nav-bar.component';
   declarations: [
     AppComponent,
     TopNavBarComponent,
+    CardComponent,
     FooterComponent
   ],
   imports: [
---
+++ src/app/card1/card1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-card',
+    templateUrl:'card1.component.html',
+    styleUrls: ['./card1.component.css']
+})
+
+export class CardComponent{
+}
