89f13529408752a472dbe033f592654512342b6e
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2021-10-18 19:31:09 -0400

    add angular integration

--- mountaineer-chat/src/environments/environment.prod.ts
+++ mountaineer-chat/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'mountaineer-chat',
+    appId: '1:883820513289:web:6a5afb07c0f72642ac50d8',
+    databaseURL: 'https://mountaineer-chat-default-rtdb.firebaseio.com',
+    storageBucket: 'mountaineer-chat.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyAuUgzQ6IeeBlUzwGr4hzeu1L3MijkYIr8',
+    authDomain: 'mountaineer-chat.firebaseapp.com',
+    messagingSenderId: '883820513289',
+    measurementId: 'G-3VMGRC0KXJ',
+  },
   production: true,
 };
