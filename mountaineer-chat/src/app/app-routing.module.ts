730f8f0bb0f6ecfc284b8db8171272b1215fe12e
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2021-10-18 18:01:51 -0400

    integrate messages page

--- mountaineer-chat/src/app/app-routing.module.ts
+++ mountaineer-chat/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SalariesComponent } from './Pages/account/account.component';
+import { AccountComponent } from './pages/account/account.component';
 
-const routes: Routes = [{ path: 'account', component: AccountComponent }, {}];
+const routes: Routes = [
+  { path: 'account', component: AccountComponent },
+  { path: '', redirectTo: '/account', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mountaineer-chat/src/app/app.module.ts
+++ mountaineer-chat/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AccountComponent } from './pages/account/account.component';
 import { FriendIconComponent } from './pages/account/friend-icon.component';
 import { ChatIconComponent } from './pages/account/chat-icon.component';
+import { MessagesComponent } from './pages/messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ChatIconComponent } from './pages/account/chat-icon.component';
     AccountComponent,
     FriendIconComponent,
     ChatIconComponent,
+    MessagesComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
