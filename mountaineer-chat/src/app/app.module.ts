01e7740295c1ef0b4d3397cfd6c9f9a17b8c157c
maccyboi24 <mma0032@mix.wvu.edu>
2021-10-18 20:18:21 -0400

    commit

--- mountaineer-chat/src/app/app.module.ts
+++ mountaineer-chat/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoggedOutComponent } from './logged-out/logged-out.component';
+import { LoggedOutComponent } from './Pages/logged-out/logged-out.component';
+
 
 @NgModule({
   declarations: [
