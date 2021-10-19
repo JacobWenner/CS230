7e9f5cdd87e6a41b96a683c0ff7f6ae89092548a
JacobWenner <jwenner13@hotmail.com>
2021-10-18 22:52:53 -0400

    Chat Members Page

---
+++ mountaineer-chat/src/app/Pages/chat-members.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-members',
+  templateUrl: './chat-members.component.html',
+  styleUrls: ['./chat-members.component.css']
+})
+export class ChatMembersComponent 
+{
+
+}
--- mountaineer-chat/src/app/app.module.ts
+++ mountaineer-chat/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatMembersComponent } from './Pages/chat-members.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatMembersComponent
   ],
   imports: [
     BrowserModule,
