c36573d43bfd27e179d1598f73692a9f2dbab71c
JacobWenner <jwenner13@hotmail.com>
2021-11-04 17:59:18 -0400

    Altered data messages on database

--- src/app/body/database.service.ts
+++ src/app/body/database.service.ts
@@ -9,14 +9,14 @@ import { UserInfo } from "./user-info.model";
 export class DatabaseService{
     items: Observable<UserInfo []>
     constructor(private db:AngularFireDatabase){
-        console.log("yo wassup");
+        console.log("Getting data from database");
         this.items = this.db.list<UserInfo>('user-info').valueChanges();
         
     }
 
     public showData(){
         this.items.subscribe((data: UserInfo []) => {
-            console.log("Da data received");
+            console.log("Data received");
             for (let item of data){
                 console.log(item);
             }
