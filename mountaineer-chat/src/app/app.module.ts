2f5e3611fc36cfa3a3df1ac0488ad145f3d2c812
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2021-11-16 16:04:03 -0500

    remove superfluous code

--- mountaineer-chat/src/app/app.module.ts
+++ mountaineer-chat/src/app/app.module.ts
@@ -2,7 +2,6 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AccountCreationComponent } from './pages/auth/create/account-creation.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
